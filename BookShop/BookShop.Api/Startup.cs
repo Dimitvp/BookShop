a3ce545b31e6704e932692ba1a7417c0df082eb0
Dimitar Nikolov <zeitgeistdimi@gmail.com>
2017-12-06 18:36:33 +0200

    Creating the DB models

--- BookShop/BookShop.Api/Startup.cs
+++ BookShop/BookShop.Api/Startup.cs
@@ -1,16 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
-
-namespace BookShop.Api
+﻿namespace BookShop.Api
 {
+    using BookShop.Data;
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.AspNetCore.Builder;
+    using Microsoft.AspNetCore.Hosting;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.DependencyInjection;
+
+
     public class Startup
     {
         public Startup(IConfiguration configuration)
@@ -23,6 +20,10 @@ namespace BookShop.Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services
+                .AddDbContext<BookShopDbContext>(options => options
+                    .UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
             services.AddMvc();
         }
 
--- BookShop/BookShop.Data/BookShopDbContext.cs
+++ BookShop/BookShop.Data/BookShopDbContext.cs
@@ -1,8 +1,47 @@
 ﻿namespace BookShop.Data
 {
+    using Models;
     using Microsoft.EntityFrameworkCore;
 
     public class BookShopDbContext : DbContext
     {
+        public BookShopDbContext(DbContextOptions<BookShopDbContext> options)
+            : base(options)
+        {
+            
+        }
+
+        public DbSet<Book> Books { get; set; }
+
+        public DbSet<Author> Authors { get; set; }
+
+        public DbSet<Category> Categories { get; set; }
+
+        public DbSet<BookCategory> BookInCategories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder
+                .Entity<BookCategory>()
+                .HasKey(bc => new {bc.BookId, bc.CategoryId});
+
+            builder
+                .Entity<Book>()
+                .HasOne(b => b.Author)
+                .WithMany(a => a.Books)
+                .HasForeignKey(b => b.AuthorId);
+
+            builder
+                .Entity<Book>()
+                .HasMany(b => b.Categories)
+                .WithOne(bc => bc.Book)
+                .HasForeignKey(bc => bc.BookId);
+
+            builder
+                .Entity<Category>()
+                .HasMany(c => c.Books)
+                .WithOne(bc => bc.Category)
+                .HasForeignKey(bc => bc.CategoryId);
+        }
     }
 }
